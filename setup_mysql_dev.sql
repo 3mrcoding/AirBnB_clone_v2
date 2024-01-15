60672aaff0f15de5953f3d6dd67c008655ac40eb
Amr Adel <amr96.adel@gmail.com>
2024-01-15 23:47:38 +0200

    TASK 3 - 1

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,4 +2,4 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON hbnb_dev_db.* TO 'performance_schema'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';

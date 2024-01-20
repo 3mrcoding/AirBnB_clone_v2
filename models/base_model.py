f935c5dba2982c49e4aeb51d1a32d67a4986c06c
Al7assan <al7assan.r@gmail.com>
2024-01-21 00:10:56 +0200

    c 7.5

--- models/base_model.py
+++ models/base_model.py
@@ -43,7 +43,7 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        """Convert Instance Into Dict format"""
+        """Convert Instance Into Dict Format"""
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':

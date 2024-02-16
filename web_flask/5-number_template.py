3324d8d34cd6009cc06f03256380c7b5a73a2691
Amr Adel <amr96.adel@gmail.com>
2024-02-16 17:11:22 +0200

    TASK 05-1

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -43,7 +43,7 @@ def num(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def int_num(n):
     """/number_template/<n>"""
-    return render_template('5-number.html', n = n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':

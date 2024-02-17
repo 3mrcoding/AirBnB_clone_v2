318ec99a3bbb665e64ee77dac567e480eae4a116
Amr Adel <amr96.adel@gmail.com>
2024-02-17 23:33:25 +0200

    TASK 10-1

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+
 from models import storage
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/states', strict_slashes=False)
-def states_list():
-    """states list"""
-    states = storage.all("State")
-    return render_template('9-states.html', states=states)
+@app.route('/states')
+def states():
+    """Get all state data"""
+    data = storage.all("State")
+    return render_template("9-states.html",
+                           states=data)
 
 
-@app.route('/states/<id>', strict_slashes=False)
-def states_list_id(id):
-    """states list"""
-    states = storage.all("State")
-    state = states.get(id)
+@app.route('/states/<id>')
+def states_by_id(id):
+    """State by id"""
     for state in storage.all("State").values():
         if state.id == id:
-            return render_template('9-states.html', states=states, state=state,
-                                   id=id, NoID=False)
-    return render_template('9-states.html', NoID=True)
+            return render_template("9-states.html",
+                                   id=id, state=state, notfound=False)
+    return render_template("9-states.html", notfound=True)
 
 
 @app.teardown_appcontext
